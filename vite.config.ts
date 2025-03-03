840643ad9e5cdec0d3dc4d068bc225d9746e9d92
Jblas7 <jbx0@outlook.es>
2025-03-03 15:37:49 +0100

    Dockerfile base
    Pendiente hacer unos cambios en el .conf del nginx para manejar algunas rutas

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
   vue()
@@ -16,7 +16,7 @@ export default defineConfig({
   },
   resolve: {
   alias: {
-  '@': fileURLToPath(new URL('./src', import.meta.url))
+    '@': path.resolve('src')
   }
   }
   })
