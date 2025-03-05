5aab9e00c385dd03544b14d7f77cc536cd710e7f
PruebaGit <n.abuin2005@gmail.com>
2025-03-05 12:15:09 +0100

    Creacion del store de sedes, con la funcion para guardar los id

--- src/stores/SedesStore.ts
+++ src/stores/SedesStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue'; 
+import { ref } from 'vue';
 
 interface Sede {
   idSede: number;
@@ -12,14 +12,32 @@ interface Sede {
 }
 
 export const useSedesStore = defineStore('sedes', () => {
+
+  //Esto crea un array que referencia al objeto que hemos creado (Sedes), el array se llamara sedes, pone ([]) Porque empezara vacio 
+
   const sedes = ref<Sede[]>([]);
+
+  //En esta variable, guarademos el id, number | null significa que puede ser un número o null, y empezara como null porq empieza vacio (null);
+
+  const selectedSedeId = ref<number | null>(null);
+
+  //Obtener todas las sedes, un fetch normal
 
   const fetchSedes = async () => {
     const response = await fetch('https://localhost:7179/api/Sedes');
     sedes.value = await response.json();
   };
 
-  fetchSedes(); // Se ejecuta automáticamente
+  //Esta sera la funcion que guarde el idSede, pidiendo un id, y el tipo (numbre)
 
-  return { sedes };
+  const selectSede = (id: number) => {
+
+    //Hace que el valor del array que hemos creado creado sea el id que pide la funcion
+
+    selectedSedeId.value = id; 
+  };
+
+  fetchSedes();
+
+  return { sedes, selectedSedeId, selectSede }; 
 });
