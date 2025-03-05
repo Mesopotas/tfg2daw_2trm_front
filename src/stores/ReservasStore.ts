7a1e7add2ef48abbc7ba50faade710acbc619f30
Jblas7 <jbx0@outlook.es>
2025-03-05 19:27:49 +0100

    Store inicial reservas

---
+++ src/stores/ReservasStore.ts
@@ -0,0 +1,46 @@
+import { defineStore } from "pinia";
+import { UserStore } from "./UserStore"; // Importamos la store de usuario
+
+interface Reserva {
+  IdReserva: number;
+  IdUsuario: number;
+  Fecha: string;
+  Descripcion: string;
+  PrecioTotal: number;
+}
+
+export const useReservasStore = defineStore("reservas", {
+  state: () => ({
+    reservas: [] as Reserva[], // array con los datos definidos en la interfaz
+  }),
+
+  actions: {
+    async createReserva(token: string, descripcion: string, precioTotal: number) {
+      const userStore = UserStore();
+      const idUsuario = userStore.user?.idUsuario; // se le asigna el id de usuario previamente almacenado en su store
+
+      if (!idUsuario) {
+        throw new Error("No se ha iniciado sesion");
+      }
+
+      const response = await fetch("https://localhost:7179/api/Reservas", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          IdUsuario: idUsuario,
+          Fecha: new Date().toISOString(),
+          Descripcion: descripcion,
+          PrecioTotal: precioTotal,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al crear la reserva.");
+      }
+
+    },
+  },
+});
