84eeba60f11cb7bf659114eb02a8b23b2317bcef
PruebaGit <n.abuin2005@gmail.com>
2025-03-06 22:20:06 +0100

    View de Disponibilidades y de Sala con sus asientos

---
+++ src/views/DisponibilidadView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Disponibilidad from '@/components/Disponibilidad.vue';
+</script>
+
+<template>
+  <div class="disponibilidad-view">
+    <Disponibilidad />
+  </div>
+</template>
+
+<style scoped>
+.disponibilidad-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+</style>
---
+++ src/views/SalaView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Sala from '../components/Sala.vue';
+
+export default {
+  components: {
+    Sala
+  }
+};
+</script>
+
+<template>
+  <div class="layout" style="background-color: #f3e7dc;">
+    <Sala />
+  </div>
+</template>
