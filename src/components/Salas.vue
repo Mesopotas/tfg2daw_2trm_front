2af5da40ebd3402eaa6458fc520e80e9335aafe0
PruebaGit <n.abuin2005@gmail.com>
2025-03-12 16:26:41 +0100

    Salas responsive

--- src/components/Salas.vue
+++ src/components/Salas.vue
@@ -31,7 +31,8 @@ salasStore.fetchSalas(sedesStore.selectedSedeId);
 
 <style scoped>
 .salas {
-  height: calc(100vh - 123px - 98px);
+  min-height: calc(100vh - 123px - 98px);
+  padding-bottom: 10px;
   text-align: center;
 
   @media (min-width: 768px) {
